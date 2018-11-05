1f8ff3def0b87a031a8e81ca49fa2ba665641aa7
Magdalena Polak <40718621+Maddalenap@users.noreply.github.com>
2018-11-05 20:30:34 +0100

    Style, delete, date
    1. Changed styles.
    2. Added "delete".
    3. Added "add/change date".

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,7 +6,7 @@ class EntriesController < ApplicationController
     @entry = Entry.find(params["id"])
   end
   def create
-    entry_params = params["entry"].permit("title", "contents")
+    entry_params = params["entry"].permit("title", "date", "contents")
     entry = Entry.create(entry_params)
     redirect_to(entry_path(entry))
   end
@@ -14,9 +14,13 @@ class EntriesController < ApplicationController
     @entry = Entry.find(params["id"])
   end
   def update
-    entry_params = params["entry"].permit("title", "contents")
+    entry_params = params["entry"].permit("title", "date", "contents")
     entry = Entry.find(params["id"])
     entry.update(entry_params)
     redirect_to(entry_path(entry))
+  end
+  def destroy
+    entry = Entry.destroy(params["id"])
+    redirect_to(entries_path, notice: 'Entry was successfully destroyed.')
   end
 end
